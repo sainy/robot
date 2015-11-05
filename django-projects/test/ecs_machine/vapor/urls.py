b12f7ca00bdaa9c00165cc8f9591aaf0561f149a
ECS system owner <administrator@ECS-b36ec859.ecs.ads.autodesk.com>
2015-11-05 01:04:11 -0800

    delete some files

--- django-projects/test/ecs_machine/vapor/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-
-from vapor import views
-
-urlpatterns = patterns('', 
-    url(r'^$', views.index, name='index'),
-    url(r'query/',views.query, name='query'),
-    
-    )
